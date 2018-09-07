01430b1cf497d8ae6e08e16665144dfe0f96bfab
Lukas Fam <takeruadelbert@gmail.com>
2018-09-07 17:06:28 +0700

    added script for download sound file (voice machine) from server to the device

---
+++ Desk Register/Download_Sound_Files/download_sound_file_config.py
@@ -0,0 +1,4 @@
+url = "http://192.168.1.201/epass-passenger/api/get-files-desk-register"
+url_download_sound_file = "http://192.168.1.201/epass-passenger/files/sounds/desk_register/"
+timeout_connection = 5 # in second(s)
+dir_sound_file = "/home/pi/python/files/sound/"
---
+++ Desk Register/Download_Sound_Files/main.py
@@ -0,0 +1,5 @@
+from download_sound_files import *
+
+if  __name__ == "__main__" :
+    sound = DownloadSoundFile()
+    sound.main()
