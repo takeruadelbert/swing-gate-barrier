f063b534c518d8a13385365e31cfae3c08cbc9f4
Lukas Fam <takeruadelbert@gmail.com>
2018-09-07 17:05:53 +0700

    update main Desk Register file

--- Desk Register/main.py
+++ Desk Register/main.py
@@ -1,9 +1,9 @@
 from desk_register import *
 
 if __name__ == "__main__" :
-    desk = DeskRegister(ip_address_server, url, timeout_connection)
+    desk = DeskRegister()
     if desk.check_server_config() :
         desk.main()
     else :
+        desk.play_sound(path_sound_file_invalid)
         print("Server configuration is not set properly.")
-    
